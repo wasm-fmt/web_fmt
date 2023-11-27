ae1f0b1d8c238381135b9347578227321c982500
magic-akari <akari.ccino@gmail.com>
2023-11-27 12:02:50 +0800

    chore: update biome config

--- crates/biome_fmt/src/config.rs
+++ crates/biome_fmt/src/config.rs
@@ -1,4 +1,4 @@
-use biome_formatter::{IndentStyle as BiomeIndentStyle, LineWidthFromIntError};
+use biome_formatter::{IndentStyle as BiomeIndentStyle, LineEnding, LineWidthFromIntError};
 use biome_js_formatter::context::JsFormatOptions;
 use biome_js_syntax::JsFileSource;
 
@@ -10,11 +10,16 @@ const TS_Config: &'static str = r#"
 export interface Config {
     indent_style?: "tab" | "space";
     indent_width?: number;
+    line_ending?: "lf" | "crlf" | "cr";
     line_width?: number;
-    quote_style?: "single" | "double";
-    quote_properties?: "as-needed" | "preserve";
+    quote_style?: "double" | "single";
+    jsx_quote_style?: "double" | "single";
+    quote_properties?: "preserve" | "as-needed";
     trailing_comma?: "es5" | "all" | "none";
-    semicolons?: "as-needed" | "always";
+    semicolons?: "always" | "as-needed";
+    arrow_parentheses?: "always" | "as-needed";
+    bracket_spacing?: boolean;
+    bracket_same_line?: boolean;
 }"#;
 
 #[derive(Deserialize, Default)]
@@ -26,11 +31,17 @@ pub(crate) struct Config {
     /// The indent width.
     indent_width: Option<u8>,
 
+    /// The type of line ending.
+    line_ending: Option<String>,
+
     /// What's the max width of a line. Defaults to 80.
     line_width: Option<u16>,
 
     /// The style for quotes. Defaults to double.
     quote_style: Option<String>,
+
+    /// The style for JSX quotes. Defaults to double.
+    jsx_quote_style: Option<String>,
 
     /// When properties in objects are quoted. Defaults to as-needed.
     quote_properties: Option<String>,
@@ -40,6 +51,15 @@ pub(crate) struct Config {
 
     /// Whether the formatter prints semicolons for all statements, class members, and type members or only when necessary because of [ASI](https://tc39.es/ecma262/multipage/ecmascript-language-lexical-grammar.html#sec-automatic-semicolon-insertion).
     semicolons: Option<String>,
+
+    /// Whether to add non-necessary parentheses to arrow functions. Defaults to "always".
+    arrow_parentheses: Option<String>,
+
+    /// Whether to insert spaces around brackets in object literals. Defaults to true.
+    bracket_spacing: Option<bool>,
+
+    /// Whether to hug the closing bracket of multiline HTML/JSX tags to the end of the last line, rather than being alone on the following line. Defaults to false.
+    bracket_same_line: Option<bool>,
 
     #[serde(skip)]
     source_type: Option<JsFileSource>,
@@ -68,6 +88,12 @@ impl TryFrom<Config> for JsFormatOptions {
             option = option.with_indent_width(indent_width.into());
         }
 
+        if let Some(line_ending) = value.line_ending {
+            let line_ending = line_ending.parse()?;
+
+            option = option.with_line_ending(line_ending);
+        };
+
         if let Some(line_width) = value.line_width {
             let line_width =
                 line_width.try_into().map_err(|e: LineWidthFromIntError| e.to_string())?;
@@ -79,6 +105,12 @@ impl TryFrom<Config> for JsFormatOptions {
             let quote_style = quote_style.parse()?;
 
             option = option.with_quote_style(quote_style);
+        };
+
+        if let Some(jsx_quote_style) = value.jsx_quote_style {
+            let jsx_quote_style = jsx_quote_style.parse()?;
+
+            option = option.with_jsx_quote_style(jsx_quote_style);
         };
 
         if let Some(quote_properties) = value.quote_properties {
@@ -97,6 +129,20 @@ impl TryFrom<Config> for JsFormatOptions {
             let semicolons = semicolons.parse()?;
 
             option = option.with_semicolons(semicolons);
+        };
+
+        if let Some(arrow_parentheses) = value.arrow_parentheses {
+            let arrow_parentheses = arrow_parentheses.parse()?;
+
+            option = option.with_arrow_parentheses(arrow_parentheses);
+        };
+
+        if let Some(bracket_spacing) = value.bracket_spacing {
+            option = option.with_bracket_spacing(bracket_spacing.into());
+        };
+
+        if let Some(bracket_same_line) = value.bracket_same_line {
+            option = option.with_bracket_same_line(bracket_same_line.into());
         };
 
         Ok(option)
